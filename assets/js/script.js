cce8a1f93605e1a236a53fabf70dc83f7c32bbfa
Corey Parker <coreyparker012@gmail.com>
2021-06-02 16:56:36 -0400

    Mostly complete. Small changes needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,133 @@
+//Weather API Key
 let apiKey = "5bf9c1a715803d8ecc4d803eb5d55d43";
 
+// Sets up the sidebar buttons and inputs
 let searchButton = $('#sidebar-button');
-let searchInput = $('#sidebar-searchbar input');
-let recentOutput = $('#recent-buttons');
+let userInputBox = $('#sidebar-searchbar input');
+let savedCities = $('#recent-buttons');
 
+// Sets up variables for the main dashboard
 let city = $('#city');
 let temp = $('#temp');
 let wind = $('#wind');
 let humidity = $('#humidity');
 let uvIndex = $('#uv-index');
+let iconT = $('#iconT');
+
+//Sets up variables for 5 day forecast
+let dayOne = $('#dayOne');
+let dayTwo = $('#dayTwo');
+let dayThree = $('#dayThree');
+let dayFour = $('#dayFour');
+let dayFive = $('#dayFive');
+
+let dayOneTemp = $('#dayOneTemp');
+let dayTwoTemp = $('#dayTwoTemp');
+let dayThreeTemp = $('#dayThreeTemp');
+let dayFourTemp = $('#dayFourTemp');
+let dayFiveTemp = $('#dayFiveTemp');
+
+let dayOneHum = $('#dayOneHum');
+let dayTwoHum = $('#dayTwoHum');
+let dayThreeHum = $('#dayThreeHum');
+let dayFourHum = $('#dayFourHum');
+let dayFiveHum = $('#dayFiveHum');
+
+let dayOneWind = $('#dayOneWind');
+let dayTwoWind = $('#dayTwoWind');
+let dayThreeWind = $('#dayThreeWind');
+let dayFourWind = $('#dayFourWind');
+let dayFiveWind = $('#dayFiveWind');
+
+let dayOneIcon = $('#dayOneIcon');
+let dayTwoIcon = $('#dayTwoIcon');
+let dayThreeIcon = $('#dayThreeIcon');
+let dayFourIcon = $('#dayFourIcon');
+let dayFiveIcon = $('#dayFiveIcon');
+
+// Sets empty arrays for future needed values
+let dailyTemps = [];
+let dailyWind = [];
+let dailyHum = [];
+let dailyIcon = [];
+let dailyIconURL = [];
+let recentCityArr = [];
+
+// Moment JS variables
+let todayDate = moment().format('L');
+let dateOne = moment(todayDate,"L").add(1, "days").format("dddd");
+let dateTwo = moment(todayDate,"L").add(2, "days").format("dddd");
+let dateThree = moment(todayDate,"L").add(3, "days").format("dddd");
+let dateFour = moment(todayDate,"L").add(4, "days").format("dddd");
+let dateFive = moment(todayDate,"L").add(5, "days").format("dddd");
+
+dayOne.html(dateOne);
+dayTwo.html(dateTwo);
+dayThree.html(dateThree);
+dayFour.html(dateFour);
+dayFive.html(dateFive);
 
 
-let recentCity = [];
-
+// Function that triggers the click event to load weather data 
 function submit() {
   
-  let cityVal = searchInput.val();
+  let userInputCity = userInputBox.val();
 
-  recentCity.push(cityVal);
+  if ($.inArray(userInputCity, recentCityArr) < 0) {
+    
+    recentCityArr.push(userInputCity);
+    localStorage.setItem('city', JSON.stringify(recentCityArr));
+    let getCoords = "https://api.openweathermap.org/data/2.5/weather?q=" + userInputCity + "&appid=" + apiKey + "&units=imperial";
+    runWeather(getCoords);
+    displayRecentCities();
 
-  localStorage.setItem('city', JSON.stringify(recentCity));
+  } else {
 
-  let getCoords = "https://api.openweathermap.org/data/2.5/weather?q=" + cityVal + "&appid=" + apiKey + "&units=imperial";
+    let getCoords = "https://api.openweathermap.org/data/2.5/weather?q=" + userInputCity + "&appid=" + apiKey + "&units=imperial";
+    runWeather(getCoords);
+    displayRecentCities();
 
-  runWeather(getCoords);
-  showRecents();
-
-}
-
-function previousCity() {
-  let lastCity = JSON.parse(localStorage.getItem('city'));
-  if (lastCity !== null) {
-      recentCity = lastCity;
   }
 }
 
-function showRecents() {
+// After submit runs, function gets length of array and displays all saved cities
+// A click event inside the functions allows you to select a previously searched city
+function displayRecentCities() {
 
-  recentOutput.empty();
+  // Prevents double posting of saved cities list
+  savedCities.empty();
 
-  for (let i = 0; i < recentCity.length; i++) {
+  for (let i = 0; i < recentCityArr.length; i++) {
 
-    let showRecents = recentCity[i];
+    let showRecents = recentCityArr[i];
 
     let createButton = $('<button>').text(showRecents);
-    recentOutput.prepend(createButton);
+
+    createButton.click(function() {
+
+        userInputBox.val(showRecents);
+        runWeather("https://api.openweathermap.org/data/2.5/weather?q=" + showRecents + "&appid=" + apiKey + "&units=imperial");
+
+    });
+
+    savedCities.prepend(createButton);
   }
-
-
 }
 
-(searchButton).click(function(event) {
+function checkForSavedCities() {
+    let lastCity = JSON.parse(localStorage.getItem('city'));
+    if (lastCity !== null) {
+        recentCityArr = lastCity;
+    }
+  }
+
+searchButton.click(function(event) {
   
   event.preventDefault();
   submit();
-  previousCity();
+  checkForSavedCities();
 
 });
-
-
-
-
-
-
-
-
 
 function runWeather(getCoords) {
 
@@ -80,31 +143,161 @@ function runWeather(getCoords) {
      city.text(data.name);
 
      let getWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey;
-     console.log(getWeather);
+     console.log(getWeather)
      
      fetch(getWeather)
      .then(function (response) {
        return response.json();
      })
      .then(function (data) {
-     
+
+        let iconToday = (data.daily[0].weather[0].icon)
+        let iconURL = "https://openweathermap.org/img/wn/" + iconToday + ".png"
+        iconT.attr('src', iconURL);
+
        temp.text(data.current.temp + '\u00B0' +'F');
        wind.text(data.current.wind_speed + ' MPH');
        humidity.text(data.current.humidity + '%');
        uvIndex.text(data.current.uvi);
 
+       if (uvIndex.text() < 4) {
+           uvIndex.css('background-color', 'green');
+           uvIndex.css('color', 'black');
+       } else if (uvIndex.text() > 8) {
+            uvIndex.css('background-color', 'red');
+            uvIndex.css('color', 'black');
+       } else {
+            uvIndex.css('background-color', 'yellow');
+            uvIndex.css('color', 'black');
+            }
+
+        for (let i = 0; i < 5; i++) {
+
+            dailyTemps[i] = data.daily[i].temp.day;
+            dailyHum[i] = data.daily[i].humidity;
+            dailyWind[i] = data.daily[i].wind_speed;
+            dailyIcon[i] = data.daily[i].weather[0].icon;
+            dailyIconURL[i] = "https://openweathermap.org/img/wn/" + dailyIcon[i] + ".png"
+
+            dayOneTemp.text(dailyTemps[0] + '\u00B0' +'F');
+            dayTwoTemp.text(dailyTemps[1] + '\u00B0' +'F');
+            dayThreeTemp.text(dailyTemps[2] + '\u00B0' +'F');
+            dayFourTemp.text(dailyTemps[3] + '\u00B0' +'F');
+            dayFiveTemp.text(dailyTemps[4] + '\u00B0' +'F');
+
+            dayOneHum.text(dailyHum[0] + '%');
+            dayTwoHum.text(dailyHum[1] + '%');
+            dayThreeHum.text(dailyHum[2] + '%');
+            dayFourHum.text(dailyHum[3] + '%');
+            dayFiveHum.text(dailyHum[4] + '%');
+
+            dayOneWind.text(dailyWind[0] + ' MPH');
+            dayTwoWind.text(dailyWind[1] + ' MPH');
+            dayThreeWind.text(dailyWind[2] + ' MPH');
+            dayFourWind.text(dailyWind[3] + ' MPH');
+            dayFiveWind.text(dailyWind[4] + ' MPH');
+
+            dayOneIcon.attr('src', dailyIconURL[0]);
+            dayTwoIcon.attr('src', dailyIconURL[1]);
+            dayThreeIcon.attr('src', dailyIconURL[2]);
+            dayFourIcon.attr('src', dailyIconURL[3]);
+            dayFiveIcon.attr('src', dailyIconURL[4]);
+
+        }
        });
-
   });
-
 }
 
+function defaultCity() {
+    
+    let getCoords = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=" + apiKey + "&units=imperial";
+
+    fetch(getCoords)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+  
+       let long = data.coord.lon;
+       let lat = data.coord.lat;
+  
+       city.text(data.name);
+  
+       let getWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey;
+       
+       
+       fetch(getWeather)
+       .then(function (response) {
+         return response.json();
+       })
+       .then(function (data) {
+
+
+        let icon = (data.daily[0].weather[0].icon)
+        let iconURL = "https://openweathermap.org/img/wn/" + icon + ".png"
+        iconT.attr('src', iconURL);
+        
+       
+         temp.text(data.current.temp + '\u00B0' +'F');
+         wind.text(data.current.wind_speed + ' MPH');
+         humidity.text(data.current.humidity + '%');
+         uvIndex.text(data.current.uvi);
+
+         if (uvIndex.text() < 4) {
+            uvIndex.css('background-color', 'green');
+            uvIndex.css('color', 'black');
+
+        } else if (uvIndex.text() > 8) {
+             uvIndex.css('background-color', 'red');
+             uvIndex.css('color', 'black');
+        } else {
+                 uvIndex.css('background-color', 'yellow');
+                 uvIndex.css('color', 'black');
+             }
+
+         for (let i = 0; i < 5; i++) {
+
+            dailyTemps[i] = data.daily[i].temp.day;
+            dailyHum[i] = data.daily[i].humidity;
+            dailyWind[i] = data.daily[i].wind_speed;
+            dailyIcon[i] = data.daily[i].weather[0].icon;
+            dailyIconURL[i] = "https://openweathermap.org/img/wn/" + dailyIcon[i] + ".png"
+
+            dayOneTemp.text(dailyTemps[0] + '\u00B0' +'F');
+            dayTwoTemp.text(dailyTemps[1] + '\u00B0' +'F');
+            dayThreeTemp.text(dailyTemps[2] + '\u00B0' +'F');
+            dayFourTemp.text(dailyTemps[3] + '\u00B0' +'F');
+            dayFiveTemp.text(dailyTemps[4] + '\u00B0' +'F');
+
+            dayOneHum.text(dailyHum[0] + '%');
+            dayTwoHum.text(dailyHum[1] + '%');
+            dayThreeHum.text(dailyHum[2] + '%');
+            dayFourHum.text(dailyHum[3] + '%');
+            dayFiveHum.text(dailyHum[4] + '%');
+
+            dayOneWind.text(dailyWind[0] + ' MPH');
+            dayTwoWind.text(dailyWind[1] + ' MPH');
+            dayThreeWind.text(dailyWind[2] + ' MPH');
+            dayFourWind.text(dailyWind[3] + ' MPH');
+            dayFiveWind.text(dailyWind[4] + ' MPH');
+
+            dayOneIcon.attr('src', dailyIconURL[0]);
+            dayTwoIcon.attr('src', dailyIconURL[1]);
+            dayThreeIcon.attr('src', dailyIconURL[2]);
+            dayFourIcon.attr('src', dailyIconURL[3]);
+            dayFiveIcon.attr('src', dailyIconURL[4]);
+
+        }
+         
+         });
+  
+    });
+
+}
 
 function init() {
-  previousCity();
-  showRecents();
+  defaultCity();
+  checkForSavedCities();
+  displayRecentCities();
 }
 init();
-
-
-
